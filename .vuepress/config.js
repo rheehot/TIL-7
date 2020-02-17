20c3ba1f657b8645f58ea9d0ce230e5d69fdca01
eyabc <bey4314@naver.com>
2020-02-18 00:08:03 +0900

    JavasScript menu config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -13,6 +13,7 @@ module.exports = {
             {
                 text: 'Development',
                 items: [
+                    { text: 'JavaScript', link: '/development/JavaScript/index'},
                     { text: 'React', link: '/development/react/index'},
                     { text: 'Vue', link: '/development/vue/makeVueBlog'},
                     { text: 'Tools', link: '/development/Tools/index'},
