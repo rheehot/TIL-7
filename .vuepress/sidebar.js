893e59da934384008446f0356ed8a91c9798b696
eyabc <bey4314@naver.com>
2020-03-18 17:33:04 +0900

    studyMoneyProject

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -5,12 +5,17 @@ const TOOLS = `${ DEVELOPMENT }/Tools`;
 const DESIGN_PATTERN = `${ DEVELOPMENT }/DesignPattern`;
 const JAVASCRIPT = `${ DEVELOPMENT }/JavaScript`;
 const REACT = `${ DEVELOPMENT }/React`;
+const INTERVIEW = `${ DEVELOPMENT}/Interview`;
+const STUDY_MONEY = `${DEVELOPMENT}/StudyMoneyProject`;
+const REACT_NATIVE = `${DEVELOPMENT}/ReactNative`;
 
 const REACT_REAL_PROGRAMMING = `${ REACT }/실전리액트프로그래밍`;
 const REACT_I18NEXT = `${ REACT }/i18next`;
 const REACT_ETC = `${ REACT }/etc`;
 
-const TOOLS_GIT = `${TOOLS}/Git`;
+const TOOLS_GIT = `${ TOOLS }/Git`;
+
+const INTERVEIW_개발상식 = `${INTERVIEW}/개발상식`;
 
 module.exports = [
 	{ title: 'Home', path: '/' },
@@ -44,10 +49,10 @@ module.exports = [
 			{
 				title: 'ETC',
 				children: [
-					{title: 'Restrict Router', path: `${REACT_ETC}/RestrictRouter`},
-					{title: 'locale 언어', path: `${REACT_ETC}/locale`},
-					{title: 'Dotenv', path: `${REACT_ETC}/Dotenv`},
-					{title: 'function component와 class component', path: `${REACT_ETC}/FunctionClassComponent`},
+					{ title: 'Restrict Router', path: `${ REACT_ETC }/RestrictRouter` },
+					{ title: 'locale 언어', path: `${ REACT_ETC }/locale` },
+					{ title: 'Dotenv', path: `${ REACT_ETC }/Dotenv` },
+					{ title: 'function component와 class component', path: `${ REACT_ETC }/FunctionClassComponent` },
 				]
 			}
 		]
@@ -55,54 +60,84 @@ module.exports = [
 	{
 		title: 'CSS',
 		children: [
-			{ title: 'CSS background-image 의 안티 패턴', path: `${CSS}/backgroundImage`}
+			{ title: 'CSS background-image 의 안티 패턴', path: `${ CSS }/backgroundImage` }
 		]
 	},
 	{
 		title: 'TERM',
 		children: [
-			{ title: 'AJAX', path: `${TERM}/AJAX`},
-			{ title: 'XHR', path: `${TERM}/XHR`},
-			{ title: '순수함수 Pure Component', path: `${TERM}/PureComponent`},
-			{ title: 'Zero-dependency Module', path: `${TERM}/ZeroDependencyModule`},
-			{ title: '명령형 프로그래밍과 함수형 프로그래밍', path: `${TERM}/JavascriptFunctionalProgramming`}
+			{ title: 'AJAX', path: `${ TERM }/AJAX` },
+			{ title: 'XHR', path: `${ TERM }/XHR` },
+			{ title: '순수함수 Pure Component', path: `${ TERM }/PureComponent` },
+			{ title: 'Zero-dependency Module', path: `${ TERM }/ZeroDependencyModule` },
+			{ title: '명령형 프로그래밍과 함수형 프로그래밍', path: `${ TERM }/JavascriptFunctionalProgramming` }
 		]
 	},
 	{
 		title: 'Tools',
 		children: [
-			{ title: '개발환경', path: `${TOOLS}/Environment`},
-			{ title: 'Markdown', path: `${TOOLS}/Markdown`},
-			{ title: 'iterm2 세팅하기', path: `${TOOLS}/Iterm2Setting`},
-			{ title: 'node.js 설치하기', path: `${TOOLS}/NodeJS`},
-			{ title: 'yarn 설치하기', path: `${TOOLS}/yarn`},
-			{ title: '웹스톰', path: ``},
-			{ title: 'vsCode', path: ``},
+			{ title: '개발환경', path: `${ TOOLS }/Environment` },
+			{ title: 'Markdown', path: `${ TOOLS }/Markdown` },
+			{ title: 'iterm2 세팅하기', path: `${ TOOLS }/Iterm2Setting` },
+			{ title: 'node.js 설치하기', path: `${ TOOLS }/NodeJS` },
+			{ title: 'yarn 설치하기', path: `${ TOOLS }/yarn` },
+			{ title: '웹스톰', path: `` },
+			{ title: 'vsCode', path: `` },
 			{
 				title: 'Git',
 				children: [
-					{title: 'Git 사용 중 자주 만나는 이슈 정리', path: `${TOOLS_GIT}/git`},
-					{title: '깃 사용자가 가장 흔히 저지르는 6가지 실수와 대처 방법', path: `${TOOLS_GIT}/MistakesGit6`},
+					{ title: 'Git 사용 중 자주 만나는 이슈 정리', path: `${ TOOLS_GIT }/git` },
+					{ title: '깃 사용자가 가장 흔히 저지르는 6가지 실수와 대처 방법', path: `${ TOOLS_GIT }/MistakesGit6` },
 				]
 			},
-			{ title: 'Ngnix', path: `${TOOLS}/Ngnix`},
-			{ title: 'https', path: `${TOOLS}/https`},
-			{ title: '인증서 mkcert', path: ``},
-			{ title: 'POSTMAN', path: ``},
-			{ title: 'api-docs spec document', path: ''},
-			{ title: 'Mac Terminal Command', path: `${TOOLS}/MacTerminalCommand`},
-			{ title: '메신저 서비스들의 메시지 관리방식', path: `${TOOLS}/ChattingService`},
+			{ title: 'Ngnix', path: `${ TOOLS }/Ngnix` },
+			{ title: 'https', path: `${ TOOLS }/https` },
+			{ title: '인증서 mkcert', path: `` },
+			{ title: 'POSTMAN', path: `` },
+			{ title: 'api-docs spec document', path: '' },
+			{ title: 'Mac Terminal Command', path: `${ TOOLS }/MacTerminalCommand` },
+			{ title: '메신저 서비스들의 메시지 관리방식', path: `${ TOOLS }/ChattingService` },
 		]
 	},
 	{
 		title: 'Design Patterns',
 		children: [
-			{ title: '상속과 조합', path: `${DESIGN_PATTERN}/CompositionAndInheritance`}
+			{ title: '상속과 조합', path: `${ DESIGN_PATTERN }/CompositionAndInheritance` }
+		]
+	},
+	{
+		title: 'Interview',
+		children: [
+			{
+				title: '개발상식',
+				children: [
+					{ title: '객체지향 프로그래밍', path: `${INTERVEIW_개발상식}/객체지향프로그래밍`},
+					{ title: 'RESTful API', path: `${INTERVEIW_개발상식}/RESTfulAPI`}
+				]
+			},
+			{ title: '네트워크', path: `` },
+			{ title: '데이터베이스', path: `` },
+			{ title: '디자인패턴', path: `` },
+			{ title: '알고리즘', path: `` },
+			{ title: '운영체제', path: `` },
+			{ title: '자료구조', path: `` },
 		]
 	},
 	{
 		title: 'ETC',
+		children: []
+	},
+	{
+		title: 'Study Money Project',
 		children: [
+			{ title: 'Intro', path: `${STUDY_MONEY}/Intro`}
+		]
+	},
+	{
+		title: 'React Native',
+		children: [
+			{ title: '우아한 형제들', path: `${REACT_NATIVE}/우아한형제들`},
+			{ title: 'React Native 시작전 고려사항', path: `${REACT_NATIVE}/ReactNative_시작전_고려사항`},
 		]
 	}
 ];
---
+++ development/Interview/개발상식/RESTfulAPI.md
@@ -0,0 +1,49 @@
+# RESTful API
+* WWW과 같은 분산 하이퍼미디어 시스템을 위한 소프트웨어 아키텍쳐의 한 형식으로
+자원을 정의하고 자원에 대한 주소를 지정하는 방법 전반에 대한 패턴
+
+* REST (Representational State Transfer)
+
+* REST의 기본원칙을 성실히 지킨 서비스 디자인
+
+* REST는 디자인 패턴이다.
+    * Resource Oriented Architecture
+    * API 설계 중심에 자원(Resource)이 있고 HTTP Method 를 통해 자원을 처리하도록 설계됨.
+    
+## REST 6 원칙
+* Uniform Interface
+* Stateless
+* Caching
+* Client-Server
+* Hierarchical system
+* Code on demand
+
+..
+## Restful 하게 API 를 디자인 한다는 것의 의미
+1. 리소스와 행위를 명시적이고 직관적으로 분리한다.
+    * 리소스는 URI 로 표현되는데 리소스가 가리키는 것은 명사로 표현되어야 한다.
+    * 행위는 HTTP Method 로 표현하고, GET(조회), POST(생성), PUT(기존 entity 전체 수정), PATCH(기존 entity 일부 수정),
+    DELETE(삭제) 를 분명한 목적으로 사용한다.
+2. Message는 Header와 Body 를 명확하게 분리해서 사용한다.
+    * Entity 에 대한 내용은 body 에 담는다.
+    * 애플리케이션 서버가 행동할 판단의 근거가 되는 컨트롤 정보인 API 버전 정보, 응답받고자 하는 MIME 타입 등은 header에 담는다.
+    * Header 와 body 는 http header 와 http body 로 나눌 수도 있고, http body 에 들어가는 json 구조로 분리할 수도 있따.
+3. API버전을 관리한다.
+    * 환경은 항상 변하기 때문에 API의 signature  가 변경될 수도 있음에 유의하자.
+    * 특정 API 를 변경할 때는 반드시 하위호환성을 보장해야 한다.
+4. 서버와 클라이언트가 같은 방식을 사용해서 요청하도록 한다.
+    * 브라우저는 form-data 형식의 submit으로 보내고 서버에서는 json 형태로 보내는 식의 분리보다는 json으로 보내든, 둘 다 form-data 형식으로 보내든 하나로 통일한다.
+    * 다른 말로 표현하자면 URI가 플랫폼 중립적이어야한다.
+    .....
+    
+## 장점
+1. Open API를 제공하기 쉽다.
+2. 멀티플랫폼 지원 및 연동이 용이하다.
+3. 원하는 타입으로 데이터를 주고 받을 수 있다.
+4. 기존 웹 인프라(HTTP)를 그대로 사용할 수 있따.
+
+## 단점
+사용할 수 있는 메소드가 4 가지 밖에 없다.
+분산환경에는 부적합하다.
+HTTP 통신 모델에 대해서만 지원한다.
+...
