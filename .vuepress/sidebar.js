d189dd6199c55054c4fa415096766abff72300ab
eyabc <bey4314@naver.com>
2020-03-22 19:12:22 +0900

    강의 정리

--- .vuepress/sidebar.js
+++ .vuepress/sidebar.js
@@ -156,7 +156,8 @@ module.exports = [
 		children: [
 			{ title: 'Mac에 Java 여러 버전 설치 & 사용하기', path: `${ JAVA }/Mac_Java_여러버전설치` }
 		]
-	}, {
+	},
+	{
 		title: 'Algorithm',
 		children: [
 			{ title: '프로그래머스 문제 모음', path: `${ALGORITHM}`}
